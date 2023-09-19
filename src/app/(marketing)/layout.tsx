61b82854fb22b7a0bf3720339709daf1e4b93ed7
SkyYap <32660903+SkyYap@users.noreply.github.com>
2023-09-20 03:03:16 +0800

    Add dark mode

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,3 +1,16 @@
+"use client"
+ 
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+ 
+import { Button } from "@/components/ui/Button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/DropdownMenu"
 import Link from 'next/link';
 
 import { cn } from '@/lib/utils/helpers';
@@ -8,6 +21,7 @@ import MainLogo from '@/components/ui/MainLogo';
 
 export default async function MarketingLayout({ children }) {
   const { routes } = config;
+  const { setTheme } = useTheme()
 
   return (
     <div className="">
@@ -23,6 +37,26 @@ export default async function MarketingLayout({ children }) {
               Login
             </Link>
           </nav>
+          <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         </div>
       </header>
       <main className="flex items-center justify-center gap-x-6 mt-6">{children}</main>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -18,7 +18,8 @@ const buttonVariants = cva(
       size: {
         default: 'h-10 py-2 px-4',
         sm: 'h-9 px-3 rounded-md',
-        lg: 'h-11 px-8 rounded-md'
+        lg: 'h-11 px-8 rounded-md',
+        icon: 'mr-2 h-4 w-4'  
       }
     },
     defaultVariants: {
